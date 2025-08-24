2d60150875db05b0ba92862a694dde6d96d5a8a0
juanm <juanmaperez5826@gmail.com>
2025-08-23 21:33:27 -0500

    first commit

---
+++ applications/app-service/src/main/java/com/crediya/authenticacion/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.crediya.authenticacion.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.crediya.authenticacion.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
