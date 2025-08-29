a6a583e4a9230f1ac94c82d8dfaec21458542d09
juanm <juanmaperez5826@gmail.com>
2025-08-28 19:07:37 -0500

    Advance

--- domain/usecase/src/main/java/com/crediya/authenticacion/usecase/exceptions/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.crediya.authenticacion.usecase.exceptions;
-
-public class DuplicateEmailException extends DomainException {
-    public DuplicateEmailException(String email) {
-        super("EMAIL_ALREADY_EXISTS", "El correo ya está registrado: " + email);
-    }
-}
